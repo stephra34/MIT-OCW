ad0f4117dce0e6e7bb4d7dd3d13db853708d8eb7
stephra34 <60305221+stephra34@users.noreply.github.com>
2020-06-01 20:05:27 -0400

    Create tetris_lesson_1.py

---
+++ tetris_lesson_1.py
@@ -0,0 +1,37 @@
+
+#Four primitive types of variables:
+#integer - a whole number. for example: 8
+#string - a bunch of characters like a sentence. for example: "hello world" 
+#float - any number including decimals. for example: 23.5
+# boolean - true or false
+
+
+# remember what i is for; i is often used because it means increment
+#for i in range(4):
+#    print(i)
+
+# this is a list of sports
+#sports = ["baseball", "football", "hockey", "soccer", "cricket"]
+#print(sports)
+
+#remember the first position is always 0; so spot 2 is hockey
+#print(sports[2])
+
+#for i in range(len(sports)):
+#    print(sports)
+#print(len(sports))
+
+#this prints out each item in the list
+#for i in range(len(sports)):
+#    print(sports[i])
+
+
+#if 1==2:
+#    print("That was true")
+#else:
+#    print("This is false")
+
+counter = 0
+while counter <10:
+    print(counter)
+    counter = counter + 1
